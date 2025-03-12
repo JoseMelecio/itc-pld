64659285dc7b5a4c2c1c6cd6f8767f68dd33d80b
Jose Refugio Melecio Soto <jrr.melecio@gmail.com>
2025-03-11 21:10:19 -0600

    Add SystemLog resource management system
    Introduce a `SystemLog` model, migration, and controller to manage logs of system-wide actions. The model supports morphable relationships and stores log details as JSON. Included a controller scaffold for CRUD operations and a migration to create the `system_logs` table.

---
+++ app/Models/SystemLog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SystemLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'model_type',
+        'model_id',
+        'type',
+        'content'
+    ];
+
+    protected $casts = [
+        'content' => 'array'
+    ];
+}
---
+++ app/Services/SystemLogService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\SystemLog;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class SystemLogService
+{
+    public static function add($data): void
+    {
+        SystemLog::create([
+            'user_id' => Auth::user()->id,
+            'model_type' => $data['model_type'],
+            'model_id' => $data['model_id'],
+            'type' => $data['type'],
+            'content' => $data['content'],
+        ]);
+    }
+}
