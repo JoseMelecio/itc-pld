878e7a06eea4f96cb8e2f852795e11a22906af4b
Jose Melecio <jrr.melecio@gmail.com>
2025-11-01 04:47:33 -0600

    ITC-XX - Simplify EBRController: remove unnecessary null checks for `ebrConfiguration` and refactor response structure accordingly.

--- app/Http/Controllers/EBRController.php
+++ app/Http/Controllers/EBRController.php
@@ -210,9 +210,7 @@ class EBRController extends Controller
         $ebrConfiguration = EBRConfiguration::where('user_id', auth()->user()->id)->first();
         $riskElements = EBRRiskElement::where('active', true)->orderBy('risk_element')->get();
 
-        if ($ebrConfiguration) {
-            $riskElementSelectedIds = $ebrConfiguration->riskElements()->pluck('ebr_risk_elements.id')->toArray();
-        }
+        $riskElementSelectedIds = $ebrConfiguration->riskElements()->pluck('ebr_risk_elements.id')->toArray();
 
         return [
             'templates' => [
@@ -220,11 +218,11 @@ class EBRController extends Controller
                 'operations' => $operationsFields,
             ],
             'ebr_configuration' => [
-                'clients' => $ebrConfiguration ? $ebrConfiguration->template_clients_config : [],
-                'operations' => $ebrConfiguration ? $ebrConfiguration->template_operations_config : [],
+                'clients' => $ebrConfiguration->template_clients_config,
+                'operations' => $ebrConfiguration->template_operations_config,
             ],
             'risk_elements' => $riskElements,
-            'risk_elements_selected' => $ebrConfiguration ? $riskElementSelectedIds : []
+            'risk_elements_selected' => $riskElementSelectedIds
         ];
     }
 }
